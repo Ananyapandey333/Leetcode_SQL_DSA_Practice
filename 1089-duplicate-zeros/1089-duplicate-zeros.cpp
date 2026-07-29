c5efed9c64e715d4c65bda4f2c74db6ae2edb154
Ananya <ananyarajeshpandey@gmail.com>
2026-07-29 22:49:50 +0530

    Time: 0 ms (100%), Space: 14.5 MB (8%) - LeetHub

---
+++ 1089-duplicate-zeros/1089-duplicate-zeros.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    void duplicateZeros(vector<int>& arr) {
+        vector<int> temp;
+        int n = arr.size();
+
+        for (int x : arr) {
+            temp.push_back(x);
+            if (x == 0) {
+                temp.push_back(0);
+            }
+        }
+
+        for (int i = 0; i < n; i++) {
+            arr[i] = temp[i];
+        }
+    }
+};
